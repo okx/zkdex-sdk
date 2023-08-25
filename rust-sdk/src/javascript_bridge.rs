aea73921cfc9ee5cf3be51c8c3bc426dd935a9ee
FineKe <530362991@qq.com>
2023-08-25 11:38:34 +0800

    update javascript sdk

--- rust-sdk/src/javascript_bridge.rs
+++ rust-sdk/src/javascript_bridge.rs
@@ -1,6 +1,7 @@
+use serde::Serialize;
 use wasm_bindgen::JsValue;
 use wasm_bindgen::prelude::wasm_bindgen;
-use crate::{hash_limit_order, hash_liquidate, hash_signed_oracle_price, hash_transfer, hash_withdraw, l1_sign, sign_limit_order, sign_liquidate, sign_signed_oracle_price, sign_transfer, sign_withdraw, verify_signature};
+use crate::{hash_limit_order, hash_liquidate, hash_signed_oracle_price, hash_transfer, hash_withdraw, is_on_curve, l1_sign, private_key_from_seed, private_key_to_pubkey_xy, sign, sign_limit_order, sign_liquidate, sign_signed_oracle_price, sign_transfer, sign_withdraw, verify_signature};
 
 #[wasm_bindgen(js_name = sign_transfer)]
 pub fn js_sign_transfer(json: &str, private_key: &str) -> Result<String, JsValue> {
@@ -115,22 +116,64 @@ pub fn js_hash_signed_oracle_price(json: &str) -> Result<String, JsValue> {
 }
 
 #[wasm_bindgen(js_name = verify_signature)]
-pub fn js_verify_signature(sig_r: &str, sig_s: &str, pub_key_x: &str, pub_key_y:&str, msg: &str) -> Result<bool,JsValue> {
-    match verify_signature(sig_r,sig_s,pub_key_x,pub_key_y,msg) {
+pub fn js_verify_signature(sig_r: &str, sig_s: &str, pub_key_x: &str, pub_key_y: &str, msg: &str) -> Result<bool, JsValue> {
+    match verify_signature(sig_r, sig_s, pub_key_x, pub_key_y, msg) {
         Ok(ret) => Ok(ret),
         Err(e) => Err(JsValue::from_str(e.to_string().as_str()))
     }
 }
 
 #[wasm_bindgen(js_name = l1_sign)]
-pub fn js_l1_sign(msg: &str, private_key: &str)-> Result<String,JsValue> {
-
-    match  l1_sign(msg,private_key) {
+pub fn js_l1_sign(msg: &str, private_key: &str) -> Result<String, JsValue> {
+    match l1_sign(msg, private_key) {
         Ok(ret) => {
             let r = serde_json::to_string(&ret).unwrap();
             Ok(r)
         }
 
+        Err(e) => Err(JsValue::from_str(e.to_string().as_str()))
+    }
+}
+
+#[wasm_bindgen(js_name = is_on_curve)]
+pub fn js_is_on_curve(pub_key_x: &str, pub_key_y: &str) -> Result<bool, JsValue> {
+    match is_on_curve(pub_key_x, pub_key_y) {
+        Ok(ret) => {
+            Ok(ret)
+        }
+        Err(e) => {
+            Err(JsValue::from_str(e.to_string().as_str()))
+        }
+    }
+}
+
+#[wasm_bindgen(js_name = sign)]
+pub fn js_sign(pri_key: &str, msg: &str) -> Result<String, JsValue> {
+    match sign(pri_key, msg) {
+        Ok(ret) => Ok(serde_json::to_string(&ret).unwrap()),
+        Err(e) => Err(JsValue::from_str(e.to_string().as_str()))
+    }
+}
+
+#[wasm_bindgen(js_name = private_key_from_seed)]
+pub fn js_private_key_from_seed(seed: &str) -> Result<String, JsValue> {
+    match private_key_from_seed(seed.as_bytes()) {
+        Ok(ret) => Ok(ret),
+        Err(e) => Err(JsValue::from_str(e.to_string().as_str()))
+    }
+}
+
+#[wasm_bindgen(js_name = private_key_to_pubkey_xy)]
+pub fn js_private_to_public_key_xy(pri_key: &str) -> Result<String, JsValue> {
+    match private_key_to_pubkey_xy(pri_key) {
+        Ok(ret) => {
+            #[derive(Serialize)]
+            struct XY {
+                x: String,
+                y: String,
+            }
+            Ok(serde_json::to_string(&XY { x: ret.0, y: ret.1 }).unwrap())
+        }
         Err(e) => Err(JsValue::from_str(e.to_string().as_str()))
     }
 }
