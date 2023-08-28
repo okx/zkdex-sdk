90e983beb719e509f0bfb98444e5e1167959f3c1
FineKe <530362991@qq.com>
2023-08-28 10:31:47 +0800

    update doc

--- rust-sdk/src/javascript_bridge.rs
+++ rust-sdk/src/javascript_bridge.rs
@@ -3,6 +3,7 @@ use wasm_bindgen::JsValue;
 use wasm_bindgen::prelude::wasm_bindgen;
 use crate::{hash_limit_order, hash_liquidate, hash_signed_oracle_price, hash_transfer, hash_withdraw, is_on_curve, l1_sign, private_key_from_seed, private_key_to_pubkey_xy, sign, sign_limit_order, sign_liquidate, sign_signed_oracle_price, sign_transfer, sign_withdraw, verify_signature};
 
+/// sign a transfer transaction
 #[wasm_bindgen(js_name = sign_transfer)]
 pub fn js_sign_transfer(json: &str, private_key: &str) -> Result<String, JsValue> {
     match sign_transfer(json, private_key) {
@@ -15,6 +16,7 @@ pub fn js_sign_transfer(json: &str, private_key: &str) -> Result<String, JsValue
     }
 }
 
+/// hash a transfer transaction
 #[wasm_bindgen(js_name = hash_transfer)]
 pub fn js_hash_transfer(json: &str) -> Result<String, JsValue> {
     match hash_transfer(json) {
@@ -27,6 +29,7 @@ pub fn js_hash_transfer(json: &str) -> Result<String, JsValue> {
     }
 }
 
+/// sign a withdraw transaction
 #[wasm_bindgen(js_name = sign_withdraw)]
 pub fn js_sign_withdraw(
     json: &str,
@@ -40,6 +43,7 @@ pub fn js_sign_withdraw(
     }
 }
 
+/// hash a withdraw transaction
 #[wasm_bindgen(js_name = hash_withdraw)]
 pub fn js_hash_withdraw(json: &str, asset_id_collateral: &str) -> Result<String, JsValue> {
     match hash_withdraw(json, asset_id_collateral) {
@@ -52,6 +56,7 @@ pub fn js_hash_withdraw(json: &str, asset_id_collateral: &str) -> Result<String,
     }
 }
 
+/// sign a limit order transaction
 #[wasm_bindgen(js_name = sign_limit_order)]
 pub fn js_sign_limit_order(json: &str, private_key: &str) -> Result<String, JsValue> {
     match sign_limit_order(json, private_key) {
@@ -64,6 +69,7 @@ pub fn js_sign_limit_order(json: &str, private_key: &str) -> Result<String, JsVa
     }
 }
 
+/// sign a limit order transaction
 #[wasm_bindgen(js_name = hash_limit_order)]
 pub fn js_hash_limit_order(json: &str) -> Result<String, JsValue> {
     match hash_limit_order(json) {
@@ -72,6 +78,7 @@ pub fn js_hash_limit_order(json: &str) -> Result<String, JsValue> {
     }
 }
 
+/// sign a liquidate transaction
 #[wasm_bindgen(js_name = sign_liquidate)]
 pub fn js_sign_liquidate(json: &str, private_key: &str) -> Result<String, JsValue> {
     match sign_liquidate(json, private_key) {
@@ -84,6 +91,7 @@ pub fn js_sign_liquidate(json: &str, private_key: &str) -> Result<String, JsValu
     }
 }
 
+/// hash a liquidate transaction
 #[wasm_bindgen(js_name = hash_liquidate)]
 pub fn js_hash_liquidate(json: &str) -> Result<String, JsValue> {
     match hash_liquidate(json) {
@@ -92,6 +100,7 @@ pub fn js_hash_liquidate(json: &str) -> Result<String, JsValue> {
     }
 }
 
+/// sign a signed oracle price transaction
 #[wasm_bindgen(js_name = sign_signed_oracle_price)]
 pub fn js_sign_signed_oracle_price(
     json: &str,
@@ -107,6 +116,7 @@ pub fn js_sign_signed_oracle_price(
     }
 }
 
+/// hash a signed oracle price transaction
 #[wasm_bindgen(js_name = hash_signed_oracle_price)]
 pub fn js_hash_signed_oracle_price(json: &str) -> Result<String, JsValue> {
     match hash_signed_oracle_price(json) {
@@ -115,6 +125,7 @@ pub fn js_hash_signed_oracle_price(json: &str) -> Result<String, JsValue> {
     }
 }
 
+/// verify a signature
 #[wasm_bindgen(js_name = verify_signature)]
 pub fn js_verify_signature(sig_r: &str, sig_s: &str, pub_key_x: &str, pub_key_y: &str, msg: &str) -> Result<bool, JsValue> {
     match verify_signature(sig_r, sig_s, pub_key_x, pub_key_y, msg) {
@@ -123,6 +134,9 @@ pub fn js_verify_signature(sig_r: &str, sig_s: &str, pub_key_x: &str, pub_key_y:
     }
 }
 
+/// l1 sign
+/// sign a msg on l1
+/// when signing a eth address
 #[wasm_bindgen(js_name = l1_sign)]
 pub fn js_l1_sign(msg: &str, private_key: &str) -> Result<String, JsValue> {
     match l1_sign(msg, private_key) {
@@ -135,6 +149,7 @@ pub fn js_l1_sign(msg: &str, private_key: &str) -> Result<String, JsValue> {
     }
 }
 
+/// check the (x,y) is on curve
 #[wasm_bindgen(js_name = is_on_curve)]
 pub fn js_is_on_curve(pub_key_x: &str, pub_key_y: &str) -> Result<bool, JsValue> {
     match is_on_curve(pub_key_x, pub_key_y) {
@@ -147,6 +162,7 @@ pub fn js_is_on_curve(pub_key_x: &str, pub_key_y: &str) -> Result<bool, JsValue>
     }
 }
 
+/// sign a msg on l2
 #[wasm_bindgen(js_name = sign)]
 pub fn js_sign(pri_key: &str, msg: &str) -> Result<String, JsValue> {
     match sign(pri_key, msg) {
@@ -155,6 +171,7 @@ pub fn js_sign(pri_key: &str, msg: &str) -> Result<String, JsValue> {
     }
 }
 
+/// derive a private key from a random seed, the seed could be anything
 #[wasm_bindgen(js_name = private_key_from_seed)]
 pub fn js_private_key_from_seed(seed: &str) -> Result<String, JsValue> {
     match private_key_from_seed(seed.as_bytes()) {
@@ -163,6 +180,7 @@ pub fn js_private_key_from_seed(seed: &str) -> Result<String, JsValue> {
     }
 }
 
+/// derive a public with xy from private key
 #[wasm_bindgen(js_name = private_key_to_pubkey_xy)]
 pub fn js_private_to_public_key_xy(pri_key: &str) -> Result<String, JsValue> {
     match private_key_to_pubkey_xy(pri_key) {
