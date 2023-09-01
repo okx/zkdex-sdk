d82cadd12a0af9fe64152f6cce65d6eae08c94d4
FineKe <530362991@qq.com>
2023-09-01 17:25:55 +0800

    add doc note for js

--- rust-sdk/src/javascript_bridge.rs
+++ rust-sdk/src/javascript_bridge.rs
@@ -3,10 +3,10 @@ use wasm_bindgen::JsValue;
 use wasm_bindgen::prelude::wasm_bindgen;
 use crate::{hash_limit_order, hash_liquidate, hash_signed_oracle_price, hash_transfer, hash_withdraw, is_on_curve, l1_sign, private_key_from_seed, private_key_to_pubkey_xy, pub_key_to_xy, sign, sign_limit_order, sign_liquidate, sign_signed_oracle_price, sign_transfer, sign_withdraw, verify_signature};
 
-/// sign a transfer transaction
-/// @param {string} json of transfer transaction
-/// @param {string} private key hex with 0x prefix
-/// @returns {string} signature of transfer transaction
+/// sign_transfer, sign a transfer transaction.
+/// @param {string} json  json of transfer transaction.
+/// @param {string} private_key private key hex with 0x prefix.
+/// @returns {string} json signature of transfer transaction.
 #[wasm_bindgen(js_name = sign_transfer, skip_jsdoc)]
 pub fn js_sign_transfer(json: &str, private_key: &str) -> Result<String, JsValue> {
     match sign_transfer(json, private_key) {
@@ -19,8 +19,10 @@ pub fn js_sign_transfer(json: &str, private_key: &str) -> Result<String, JsValue
     }
 }
 
-/// hash a transfer transaction
-#[wasm_bindgen(js_name = hash_transfer)]
+/// hash_transfer, hash a transfer transaction.
+/// @param {string} json  json of transfer transaction.
+/// @returns {string} string hash of transfer transaction with 0x prefix.
+#[wasm_bindgen(js_name = hash_transfer, skip_jsdoc)]
 pub fn js_hash_transfer(json: &str) -> Result<String, JsValue> {
     match hash_transfer(json) {
         Ok(ret) => {
@@ -32,8 +34,12 @@ pub fn js_hash_transfer(json: &str) -> Result<String, JsValue> {
     }
 }
 
-/// sign a withdraw transaction
-#[wasm_bindgen(js_name = sign_withdraw)]
+
+/// sign_withdraw, sign a withdraw transaction.
+/// @param {string} json  json of withdraw transaction.
+/// @param {string} private_key private key hex with 0x prefix.
+/// @returns {string} json signature of withdraw transaction.
+#[wasm_bindgen(js_name = sign_withdraw, skip_jsdoc)]
 pub fn js_sign_withdraw(
     json: &str,
     private_key: &str,
@@ -45,8 +51,10 @@ pub fn js_sign_withdraw(
     }
 }
 
-/// hash a withdraw transaction
-#[wasm_bindgen(js_name = hash_withdraw)]
+/// hash_withdraw, hash a withdraw transaction.
+/// @param {string} json  json of withdraw transaction.
+/// @returns {string} string hash of withdraw transaction with 0x prefix.
+#[wasm_bindgen(js_name = hash_withdraw,skip_jsdoc)]
 pub fn js_hash_withdraw(json: &str) -> Result<String, JsValue> {
     match hash_withdraw(json) {
         Ok(ret) => {
@@ -58,8 +66,11 @@ pub fn js_hash_withdraw(json: &str) -> Result<String, JsValue> {
     }
 }
 
-/// sign a limit order transaction
-#[wasm_bindgen(js_name = sign_limit_order)]
+/// sign_limit_order, sign a limit order transaction.
+/// @param {string} json  json of limit order transaction.
+/// @param {string} private_key private key hex with 0x prefix.
+/// @returns {string} json signature of limit order transaction.
+#[wasm_bindgen(js_name = sign_limit_order, skip_jsdoc)]
 pub fn js_sign_limit_order(json: &str, private_key: &str) -> Result<String, JsValue> {
     match sign_limit_order(json, private_key) {
         Ok(ret) => {
@@ -71,8 +82,10 @@ pub fn js_sign_limit_order(json: &str, private_key: &str) -> Result<String, JsVa
     }
 }
 
-/// sign a limit order transaction
-#[wasm_bindgen(js_name = hash_limit_order)]
+/// hash_limit_order, sign a limit order transaction.
+/// @param {string} json  json of limit order transaction.
+/// @returns {string} string hash of limit order transaction with 0x prefix.
+#[wasm_bindgen(js_name = hash_limit_order, skip_jsdoc)]
 pub fn js_hash_limit_order(json: &str) -> Result<String, JsValue> {
     match hash_limit_order(json) {
         Ok(ret) => Ok(ret),
@@ -80,8 +93,11 @@ pub fn js_hash_limit_order(json: &str) -> Result<String, JsValue> {
     }
 }
 
-/// sign a liquidate transaction
-#[wasm_bindgen(js_name = sign_liquidate)]
+/// sign_liquidate, sign a liquidate transaction.
+/// @param {string} json  json of liquidate transaction.
+/// @param {string} private_key private key hex with 0x prefix.
+/// @returns {string} json signature of liquidate transaction.
+#[wasm_bindgen(js_name = sign_liquidate, skip_jsdoc)]
 pub fn js_sign_liquidate(json: &str, private_key: &str) -> Result<String, JsValue> {
     match sign_liquidate(json, private_key) {
         Ok(ret) => {
@@ -93,8 +109,10 @@ pub fn js_sign_liquidate(json: &str, private_key: &str) -> Result<String, JsValu
     }
 }
 
-/// hash a liquidate transaction
-#[wasm_bindgen(js_name = hash_liquidate)]
+/// hash_liquidate, hash a liquidate transaction.
+/// @param {string} json  json of liquidate transaction.
+/// @returns {string} string hash of liquidate transaction with 0x prefix.
+#[wasm_bindgen(js_name = hash_liquidate, skip_jsdoc)]
 pub fn js_hash_liquidate(json: &str) -> Result<String, JsValue> {
     match hash_liquidate(json) {
         Ok(ret) => Ok(ret),
@@ -102,8 +120,11 @@ pub fn js_hash_liquidate(json: &str) -> Result<String, JsValue> {
     }
 }
 
-/// sign a signed oracle price transaction
-#[wasm_bindgen(js_name = sign_signed_oracle_price)]
+/// sign_signed_oracle_price, sign a signed oracle price transaction.
+/// @param {string} json  json of liquidate transaction.
+/// @param {string} private_key private key hex with 0x prefix.
+/// @returns {string} json signature of liquidate transaction.
+#[wasm_bindgen(js_name = sign_signed_oracle_price, skip_jsdoc)]
 pub fn js_sign_signed_oracle_price(
     json: &str,
     private_key: &str,
@@ -118,8 +139,10 @@ pub fn js_sign_signed_oracle_price(
     }
 }
 
-/// hash a signed oracle price transaction
-#[wasm_bindgen(js_name = hash_signed_oracle_price)]
+/// hash_signed_oracle_price, hash a signed oracle price transaction.
+/// @param {string} json  json of signed oracle transaction.
+/// @returns {string} string hash of signed oracle transaction with 0x prefix.
+#[wasm_bindgen(js_name = hash_signed_oracle_price, skip_jsdoc)]
 pub fn js_hash_signed_oracle_price(json: &str) -> Result<String, JsValue> {
     match hash_signed_oracle_price(json) {
         Ok(ret) => Ok(ret),
@@ -127,8 +150,14 @@ pub fn js_hash_signed_oracle_price(json: &str) -> Result<String, JsValue> {
     }
 }
 
-/// verify a signature
-#[wasm_bindgen(js_name = verify_signature)]
+/// verify_signature, verify a signature.
+/// @param {string} sig_r  r of signature.
+/// @param {string} sig_s  s of signature.
+/// @param {string} pub_key_x  x of public key.
+/// @param {string} pub_key_y  y of public key.
+/// @param {string} msg  msg hex with 0x prefix.
+/// @returns {bool} whether the signature is valid.
+#[wasm_bindgen(js_name = verify_signature, skip_jsdoc)]
 pub fn js_verify_signature(sig_r: &str, sig_s: &str, pub_key_x: &str, pub_key_y: &str, msg: &str) -> Result<bool, JsValue> {
     match verify_signature(sig_r, sig_s, pub_key_x, pub_key_y, msg) {
         Ok(ret) => Ok(ret),
@@ -136,9 +165,11 @@ pub fn js_verify_signature(sig_r: &str, sig_s: &str, pub_key_x: &str, pub_key_y:
     }
 }
 
-/// l1 sign
-/// sign a msg on l1 when signing a eth address
-#[wasm_bindgen(js_name = l1_sign)]
+/// l1 sign, sign a msg on l1 when signing a eth address.
+/// @param {string} msg  msg coding in hex with 0x prefix.
+/// @param {string} private_key private key hex with 0x prefix.
+/// @param {string} string of signature.
+#[wasm_bindgen(js_name = l1_sign, skip_jsdoc)]
 pub fn js_l1_sign(msg: &str, private_key: &str) -> Result<String, JsValue> {
     match l1_sign(msg, private_key) {
         Ok(ret) => {
@@ -150,8 +181,11 @@ pub fn js_l1_sign(msg: &str, private_key: &str) -> Result<String, JsValue> {
     }
 }
 
-/// check the (x,y) is on curve
-#[wasm_bindgen(js_name = is_on_curve)]
+/// is_on_curve, check the (x,y) is on curve.
+/// @param {string} pub_key_x  x of public key with 0x prefix.
+/// @param {string} pub_key_y  y of public key with 0x prefix.
+/// @returns {bool} whether the (x,y) is on curve.
+#[wasm_bindgen(js_name = is_on_curve, skip_jsdoc)]
 pub fn js_is_on_curve(pub_key_x: &str, pub_key_y: &str) -> Result<bool, JsValue> {
     match is_on_curve(pub_key_x, pub_key_y) {
         Ok(ret) => {
@@ -163,17 +197,22 @@ pub fn js_is_on_curve(pub_key_x: &str, pub_key_y: &str) -> Result<bool, JsValue>
     }
 }
 
-/// sign a msg on l2
-#[wasm_bindgen(js_name = sign)]
-pub fn js_sign(pri_key: &str, msg: &str) -> Result<String, JsValue> {
-    match sign(pri_key, msg) {
+/// sign, sign a msg on l2, is a generic signature methods.
+/// @param {string} msg  msg coding in hex with 0x prefix.
+/// @param {string} private_key private key hex with 0x prefix.
+/// @returns {string} json string of the signature.
+#[wasm_bindgen(js_name = sign, skip_jsdoc)]
+pub fn js_sign(private_key: &str, msg: &str) -> Result<String, JsValue> {
+    match sign(private_key, msg) {
         Ok(ret) => Ok(serde_json::to_string(&ret).unwrap()),
         Err(e) => Err(JsValue::from_str(e.to_string().as_str()))
     }
 }
 
-/// derive a private key from a random seed, the seed could be anything
-#[wasm_bindgen(js_name = private_key_from_seed)]
+/// private_key_from_seed, derive a private key from a random seed, the seed could be anything.
+/// @param {string} seed  anything string.
+/// @returns {string} string of private coding in hex with 0x prefix.
+#[wasm_bindgen(js_name = private_key_from_seed, skip_jsdoc)]
 pub fn js_private_key_from_seed(seed: &str) -> Result<String, JsValue> {
     match private_key_from_seed(seed.as_bytes()) {
         Ok(ret) => Ok(ret),
@@ -181,8 +220,10 @@ pub fn js_private_key_from_seed(seed: &str) -> Result<String, JsValue> {
     }
 }
 
-/// derive a public with xy from private key
-#[wasm_bindgen(js_name = private_key_to_pubkey_xy)]
+/// private_key_to_pubkey_xy, derive a public with xy from private key.
+/// @param {string} private_key private key hex with 0x prefix.
+/// @returns {string} json string of public key xy.
+#[wasm_bindgen(js_name = private_key_to_pubkey_xy, skip_jsdoc)]
 pub fn js_private_to_public_key_xy(pri_key: &str) -> Result<String, JsValue> {
     match private_key_to_pubkey_xy(pri_key) {
         Ok(ret) => {
@@ -197,8 +238,10 @@ pub fn js_private_to_public_key_xy(pri_key: &str) -> Result<String, JsValue> {
     }
 }
 
-/// convert public key to xy
-#[wasm_bindgen(js_name = public_key_to_xy)]
+/// public_key_to_xy, convert public key to xy.
+/// @param {string} pub_key public key hex with 0x prefix.
+/// @returns {string} json string of public key xy.
+#[wasm_bindgen(js_name = public_key_to_xy, skip_jsdoc)]
 pub fn js_public_key_to_xy(pub_key: &str) -> Result<String, JsValue> {
     match pub_key_to_xy(pub_key) {
         Ok(ret) => {
