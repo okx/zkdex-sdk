c038229124211a2371622681042ec0af49077635
fan.ke <fan.ke@okg.com>
2023-12-06 18:40:06 +0800

    fix

--- rust-sdk/build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let version = if let Ok(version) = std::env::var("CARGO_PKG_VERSION") {
-        version
-    } else {
-        "unknown".to_string()
-    };
-
-    println!("cargo:rustc-env=APP_VERSION={}", version);
-}
