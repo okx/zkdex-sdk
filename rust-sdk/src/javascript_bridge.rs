d1685f1678a5e080b1c443401893d0cf7c731d93
fan.ke <fan.ke@okg.com>
2024-04-17 11:10:19 +0800

    fix

--- rust-sdk/src/javascript_bridge.rs
+++ rust-sdk/src/javascript_bridge.rs
@@ -1,14 +1,7 @@
 #[cfg(feature = "js")]
 pub mod javascript_bridge {
     use crate::utils::set_panic_hook;
-    use crate::{
-        hash_limit_order, hash_liquidate, hash_signed_oracle_price, hash_spot_limit_order,
-        hash_spot_transfer, hash_spot_withdrawal, hash_transfer, hash_withdraw, is_on_curve,
-        l2_sign, private_key_from_seed, private_key_to_pubkey_xy, pub_key_to_xy, sign,
-        sign_limit_order, sign_liquidate, sign_signed_oracle_price, sign_spot_limit_order,
-        sign_spot_transfer, sign_spot_withdrawal, sign_transfer, sign_withdraw, verify_signature,
-        JUBJUB_PARAMS, RESCUE_PARAMS,
-    };
+    use crate::{hash_limit_order, hash_liquidate, hash_signed_oracle_price, hash_spot_limit_order, hash_spot_transfer, hash_spot_withdrawal, hash_transfer, hash_withdraw, is_on_curve, l2_sign, private_key_from_seed, private_key_to_pubkey_xy, pub_key_to_xy, sign, sign_limit_order, sign_liquidate, sign_signed_oracle_price, sign_spot_limit_order, sign_spot_transfer, sign_spot_withdrawal, sign_transfer, sign_withdraw, verify_signature, JUBJUB_PARAMS, RESCUE_PARAMS, sign_eth_address};
     use serde::Serialize;
     use wasm_bindgen::prelude::wasm_bindgen;
     use wasm_bindgen::JsValue;
